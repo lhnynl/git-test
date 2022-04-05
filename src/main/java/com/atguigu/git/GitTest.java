27b13ba2febbe39bcf75374e69eed23b80131736
lhn <2014954846@qq.com>
2022-04-05 16:29:06 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git git");
         System.out.println("hello git git git");
+        System.out.println("xiu gai hot-fix fen zhi");
     }
 }
